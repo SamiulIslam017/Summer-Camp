ac45e50128a6c5736c5b78550eac69d2d3e36492
SamiulIslam017 <samiul641@gmail.com>
2023-06-11 21:09:22 +0600

    Instructor section Updated

--- src/pages/Home/InstructorsSection/InstructorSectionCard.jsx
+++ src/pages/Home/InstructorsSection/InstructorSectionCard.jsx
@@ -1,31 +1,33 @@
+
 import { useEffect, useState } from "react";
 
 const InstructorSectionCard = () => {
+
     const [instructors, setInstructors] = useState([]);
+
+
     useEffect(() => {
         fetch(`${import.meta.env.VITE_DOMAIN}/instructors`)
             .then(res => res.json())
             .then(data => setInstructors(data))
     }, [])
+
     const allInstructors = instructors.filter(instructor => instructor.role == "instructor")
-    console.log(allInstructors);
+
+
     return (
         <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-6">
-            {allInstructors.slice(0.6).map(instructor =>
-                <div className="card shadow-xl" key={instructor._id}>
-                    <figure><img src={instructor.image} className="h-[260px]" /></figure>
-                    <div className="card-body">
-                        <h2 className="card-title">
-                            {instructor.name}
-                        </h2>
-                        {/* TODO */}
-                        <p>Total Courses: 0</p>
-                        <div className="card-actions justify-center mt-6">
-                            <button className="btn bg-blue hover:bg-transparent text-neutral-100 hover:border-blue hover:text-blue">All Courses</button>
+            {allInstructors.slice(0, 6).map(instructor =>
+                <div key={instructor._id} className="hero min-h-[150px] rounded-md" style={{ backgroundImage: `url(${instructor.image})` }}>
+                    <div className="hero-overlay bg-opacity-60 rounded-md"></div>
+                    <div className="hero-content text-center text-neutral-content">
+                        <div className="max-w-md">
+                            <h1 className="mb-5 text-xl font-bold">{instructor.name}</h1>
                         </div>
                     </div>
                 </div>
             )}
+
         </div>
     );
 };
