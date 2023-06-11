cee250d18dbc3ad53323f35fbcdc27e068e3e1e1
SamiulIslam017 <samiul641@gmail.com>
2023-06-12 01:53:19 +0600

    update some loading effect

--- src/components/CourseCard.jsx
+++ src/components/CourseCard.jsx
@@ -5,6 +5,7 @@ import { AuthContext } from "../Provider/AuthProvider";
 const CourseCard = () => {
 
     const [courses, setCourses] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
     const { user } = useContext(AuthContext);
     const [hidden, setHidden] = useState({ user })
     useEffect(() => {
@@ -13,6 +14,7 @@ const CourseCard = () => {
             .then(data => {
 
                 setHidden(data)
+                setIsLoading(false)
             }
             )
     }, [user])
@@ -23,28 +25,45 @@ const CourseCard = () => {
             .then(data => {
 
                 setCourses(data)
+                setIsLoading(false)
             })
     }, [])
     return (
-        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-8 mb-10 md:mb-20 lg:mb-20">
+        <>
             {
-                courses.map(course => <div key={course._id} className={`card glass ${course.total_seats < 1 && 'bg-orange opacity-50'}`}>
-                    <figure><img className="h-[200px] w-full object-cover" src={course.image} /></figure>
-                    <div className="card-body">
-                        <h2 className="card-title">{course.course_name}</h2>
-                        <p>Instructor: {course.instructor_name}</p>
-                        <div className="flex justify-between my-5">
+                isLoading ? <div className="min-h-screen flex justify-center items-center"><img src="https://i.ibb.co/GMCwfS6/loading-spinner.gif" /></div> :
+                    <>
+                        <div className="hero h-[300px] mb-10 md:mb-20 lg:mb-20" style={{ backgroundImage: `url('https://i.ibb.co/p0Vttrh/cool-background.png')` }}>
+                            <div className="hero-overlay bg-opacity-60"></div>
+                            <div className="hero-content text-center text-neutral-content">
+                                <div className="max-w-md">
+                                    <h1 className="mb-5 text-5xl font-bold">Welcome To Our Course Page</h1>
+                                    <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-8 mb-10 md:mb-20 lg:mb-20">
+                            {
+                                courses.map(course => <div key={course._id} className={`card glass ${course.total_seats < 1 && 'bg-orange opacity-50'}`}>
+                                    <figure><img className="h-[200px] w-full object-cover" src={course.image} /></figure>
+                                    <div className="card-body">
+                                        <h2 className="card-title">{course.course_name}</h2>
+                                        <p>Instructor: {course.instructor_name}</p>
+                                        <div className="flex justify-between my-5">
 
-                            <p className="font-bold text-xl">Available Seats: {course.total_seats}</p>
-                            <p className="font-bold text-orange text-xl text-right">$ {course.price}</p>
+                                            <p className="font-bold text-xl">Available Seats: {course.total_seats}</p>
+                                            <p className="font-bold text-orange text-xl text-right">$ {course.price}</p>
+                                        </div>
+                                        <div className="card-actions justify-center">
+                                            <button className="btn bg-blue text-neutral-100 border-0 hover:bg-transparent hover:text-blue hover:border hover:border-blue" disabled={hidden.role === "instructor" || hidden.role === "admin" || course.total_seats < 1 ? 'disable' : ''}>Join Course</button>
+                                        </div>
+                                    </div>
+                                </div>)
+                            }
                         </div>
-                        <div className="card-actions justify-center">
-                            <button className="btn bg-blue text-neutral-100 border-0 hover:bg-transparent hover:text-blue hover:border hover:border-blue" disabled={hidden.role === "instructor" || hidden.role === "admin" || course.total_seats < 1 ? 'disable' : ''}>Join Course</button>
-                        </div>
-                    </div>
-                </div>)
+                    </>
             }
-        </div>
+        </>
     );
 };
 
--- src/pages/AllInstructor/AllInstructor.jsx
+++ src/pages/AllInstructor/AllInstructor.jsx
@@ -1,18 +1,12 @@
+
 import InstructorCard from "../../components/InstructorCard";
 
 
 const AllInstructor = () => {
+
     return (
         <>
-            <div className="hero h-[300px] mb-10 md:mb-20 lg:mb-20" style={{ backgroundImage: `url('https://i.ibb.co/p0Vttrh/cool-background.png')` }}>
-                <div className="hero-overlay bg-opacity-60"></div>
-                <div className="hero-content text-center text-neutral-content">
-                    <div className="max-w-md">
-                        <h1 className="mb-5 text-5xl font-bold">Our Master Class Instructors</h1>
-                        <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    </div>
-                </div>
-            </div>
+
             <InstructorCard></InstructorCard>
         </>
     );
